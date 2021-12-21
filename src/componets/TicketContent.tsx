20e7040de420851964ea0b255f4412f1d14bcfaf
Dmitrii S <74116697+dimsp1369@users.noreply.github.com>
2021-12-20 20:53:15 -0800

    round duration and time fix readme

--- src/componets/TicketContent.tsx
+++ src/componets/TicketContent.tsx
@@ -11,29 +11,28 @@ const TicketContent: FC<TicketSegmentProps> = ({segment}) => {
     const departureTime = (date: string) => {
         const departureDate = new Date(date)
         const hours = departureDate.getUTCHours()
-        let minutes = departureDate.getUTCMinutes()
-        return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`
+        let minutes = Math.round(departureDate.getUTCMinutes() / 10) * 10
+        return `${hours}:${minutes}`
     }
 
     const arrivingTime = (date: string, duration: number) => {
         const arrivingDate = new Date(Date.parse(date) + duration * 60 * 1000)
         const hours = arrivingDate.getUTCHours()
-        let minutes = arrivingDate.getUTCMinutes()
-        return `${hours}:${minutes < 10 ? '0' + minutes : minutes}`
+        let minutes = Math.round(arrivingDate.getUTCMinutes() / 10) * 10
+        return `${hours}:${minutes === 0 ? '00' : minutes}`
     }
 
     return (
         <CardContent>
-            <Grid container justifyContent={'space-between'} >
+            <Grid container justifyContent={'space-between'}>
                 <Grid item xs='auto'>
-                    <Typography color="text.secondary" gutterBottom variant={'subtitle2'} sx={{fontSize: '12px', marginBottom: 0,}}>
+                    <Typography color="text.secondary" gutterBottom variant={'subtitle2'}
+                                sx={{fontSize: '12px', marginBottom: 0,}}>
                         {segment.origin} - {segment.destination}
                     </Typography>
-                    {/*<Badge badgeContent={`+${(Math.floor(segment.duration / 1440)).toFixed()}`}>*/}
                     <Typography variant={'subtitle1'} component={'div'}>
                         {`${departureTime(segment.date)} - ${arrivingTime(segment.date, segment.duration)}`}
                     </Typography>
-                    {/*</Badge>*/}
                 </Grid>
                 <Grid item xs='auto'>
                     <Typography color="text.secondary" gutterBottom sx={{fontSize: '12px', marginBottom: 0,}}>
@@ -41,7 +40,8 @@ const TicketContent: FC<TicketSegmentProps> = ({segment}) => {
                     </Typography>
                     <Typography>
                         {Math.floor(segment.duration / 60)} h {' '}
-                        {Math.round(((segment.duration / 60) - (Math.floor(segment.duration / 60))) * 60)} m
+                        {Math.round((Math.round(((segment.duration / 60)
+                            - (Math.floor(segment.duration / 60))) * 60)) / 10) * 10} m
                     </Typography>
                 </Grid>
                 <Grid item xs='auto' alignSelf={'center'} textAlign={'center'}>
